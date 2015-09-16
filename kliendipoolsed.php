74752f3c438305638554a29f4ec71dfab7e9e4aa
alvarpungar <alvar.pungar@khk.ee>
2015-09-16 13:23:06 +0300

    3. Tere maailm link [Delivers #103481950]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -5,11 +5,12 @@
     <title>Pungar</title>
 </head>
 <body>
-    <button onclick="myFunction()">Tere Maailm</button>
+    <button onclick="myFunction()">Tere maailm</button><br>
     <script>
         function myFunction() {
-            alert("Tere Maailm!");
+            alert("Tere maailm!");
         }
     </script>
+    <a href="http://www.khk.ee" onClick="alert('Tere maailm!')">Tere maailm</a>
 </body>
 </html>
