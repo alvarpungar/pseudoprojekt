48d779aea0bd58846eef0162f8710f2c30698fe2
alvarpungar <alvar.pungar@khk.ee>
2015-09-16 13:07:55 +0300

    2. Tere maailm button [Delivers #103481924]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -5,5 +5,11 @@
     <title>Pungar</title>
 </head>
 <body>
+    <button onclick="myFunction()">Tere Maailm</button>
+    <script>
+        function myFunction() {
+            alert("Tere Maailm!");
+        }
+    </script>
 </body>
 </html>
