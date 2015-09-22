d8333aa349b0a62f792cdc887eccf1f7e547e16c
alvarpungar <alvar.pungar@khk.ee>
2015-09-22 10:30:20 +0300

    5. Kass koeraks [Delivers #103878432]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -12,7 +12,20 @@
         }
     </script>
     <a href="http://www.khk.ee" onClick="alert('Tere maailm!')">Tere maailm</a><br>
-    <a href="http://www.khk.ee" onClick="alert('Jääme siia!') ;return false">Jääme siia</a>
+    <a href="http://www.khk.ee" onClick="alert('Jääme siia!');return false">Jääme siia</a><br>
+    <img alt="" src="http://images.medicaldaily.com/sites/medicaldaily.com/files/2013/08/04/0/62/6259.jpg"
+         style="height: 300px; width: 400px" id="imgClickAndChange" onclick="changeImage()"/>
+    <script language="javascript">
+        function changeImage() {
+
+            if (document.getElementById("imgClickAndChange").src == "http://images.medicaldaily.com/sites/medicaldaily.com/files/2013/08/04/0/62/6259.jpg") {
+                document.getElementById("imgClickAndChange").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7juIqQj7w7nZpj4FCqMrmw4tGPXnUzRL3JQGZ7Xcu9r-KClrA9QHE8KqZ";
+            }
+            else {
+                document.getElementById("imgClickAndChange").src = "http://images.medicaldaily.com/sites/medicaldaily.com/files/2013/08/04/0/62/6259.jpg";
+            }
+        }
+    </script>
 
 </body>
 </html>
