09c678156cd78cc520213134f6e5fa61592a45bc
alvarpungar <alvar.pungar@khk.ee>
2015-09-23 14:57:34 +0300

    6. jQuery kass koeraks [Delivers #103878434]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Pungar</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
 <body>
     <button onclick="myFunction()">Tere maailm</button><br>
@@ -13,19 +14,16 @@
     </script>
     <a href="http://www.khk.ee" onClick="alert('Tere maailm!')">Tere maailm</a><br>
     <a href="http://www.khk.ee" onClick="alert('Jääme siia!');return false">Jääme siia</a><br>
-    <img alt="" src="http://images.medicaldaily.com/sites/medicaldaily.com/files/2013/08/04/0/62/6259.jpg"
-         style="height: 300px; width: 400px" id="imgClickAndChange" onclick="changeImage()"/>
-    <script language="javascript">
-        function changeImage() {
 
-            if (document.getElementById("imgClickAndChange").src == "http://images.medicaldaily.com/sites/medicaldaily.com/files/2013/08/04/0/62/6259.jpg") {
-                document.getElementById("imgClickAndChange").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7juIqQj7w7nZpj4FCqMrmw4tGPXnUzRL3JQGZ7Xcu9r-KClrA9QHE8KqZ";
-            }
-            else {
-                document.getElementById("imgClickAndChange").src = "http://images.medicaldaily.com/sites/medicaldaily.com/files/2013/08/04/0/62/6259.jpg";
-            }
-        }
+    <div class="kass2">
+        <img src="http://images.medicaldaily.com/sites/medicaldaily.com/files/2013/08/04/0/62/6259.jpg" width="20%"
+             height="20%">
+    </div>
+
+    <script>
+        $("img[src='http://images.medicaldaily.com/sites/medicaldaily.com/files/2013/08/04/0/62/6259.jpg']").click(function () {
+            $(this).attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7juIqQj7w7nZpj4FCqMrmw4tGPXnUzRL3JQGZ7Xcu9r-KClrA9QHE8KqZ")
+        })
     </script>
-
 </body>
 </html>
