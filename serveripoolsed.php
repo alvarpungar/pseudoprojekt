bad9743401cd7cae41313519ff4c1f6b85d4e072
alvarpungar <alvar.pungar@khk.ee>
2015-10-27 11:17:43 +0200

    18. Objektid [Delivers #103878460]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -2,15 +2,20 @@
 
 $eesnimi_algab_vokaaliga = true;
 
-$isik = array(
-    "eesnimi" => "Alvar",
-    "perenimi" => "Pungar",
-    "vanus" => "24",
-);
+class inimene
+{
+    var $eesnimi = "Alvar";
+    var $perenimi = "Pungar";
+    var $vanus = 24;
+    var $sugu = "Mees";
+}
+
+$Pung = new inimene;
 
 if ($eesnimi_algab_vokaaliga) {
     echo "<p>Nimi algab vokaaliga</p>";
 }
 
-print  "<p>" . $isik['eesnimi'] . " " . $isik['perenimi'] . " (" . $isik['vanus'] . ")</p>";
+
+print  "<p>" . $Pung->eesnimi . " " . $Pung->perenimi . " (" . $Pung->vanus . ") " . $Pung->sugu . "</p>";
 ?>
