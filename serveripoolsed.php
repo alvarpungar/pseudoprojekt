67790d3e85ab172d118033d7b397709845e28395
alvarpungar <alvar.pungar@khk.ee>
2015-10-27 11:11:12 +0200

    17. Tingimused [Delivers #103878458]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,10 +1,16 @@
 <?php
 
+$eesnimi_algab_vokaaliga = true;
+
 $isik = array(
     "eesnimi" => "Alvar",
     "perenimi" => "Pungar",
     "vanus" => "24",
 );
-$eesnimi_algab_vokaaliga = true;
+
+if ($eesnimi_algab_vokaaliga) {
+    echo "<p>Nimi algab vokaaliga</p>";
+}
+
 print  "<p>" . $isik['eesnimi'] . " " . $isik['perenimi'] . " (" . $isik['vanus'] . ")</p>";
 ?>
