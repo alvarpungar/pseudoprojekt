428cb931b62ffa7451674972f9b976c6023fd632
alvarpungar <alvar.pungar@khk.ee>
2015-11-01 17:37:25 +0200

    22. $_POST [Delivers #103878468]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,27 +1,11 @@
+<form action="?" method="POST">
+    <label for="kass">Sisesta kass:</label> <input type="text" id="kass" name="kass">
+</form>
+
 <?php
-class inimene
-{
-    var $eesnimi = "Alvar";
-    var $perenimi = "Pungar";
-    var $vanus = 24;
-    var $sugu = "Mees";
+
+if (!empty($_POST['kass'])) {
+    echo "Kass oli: " . $_POST['kass'];
 }
 
-$person = new inimene;
-
-$vowels = array('A', 'E', 'I', 'O', 'U', 'Õ', 'Ä', 'Ö', 'Ü', 'a', 'e', 'i', 'o', 'u', 'õ', 'ä', 'ö', 'ü');
-if (in_array(substr($person->eesnimi, 0, 1), $vowels)) {
-    $eesnimi_algab_vokaaliga = true;
-} else {
-    $eesnimi_algab_vokaaliga = false;
-}
-
-$floating1 = 1.23;
-$floating2 = 2.34;
-$float_sum = $floating1 + $floating2;
-
-
-echo "<p>" . $float_sum . "</p>";
-
-echo $_GET['koer'];
 ?>
