31d9e1305922cb12a002c43bd3677997f3682d1f
alvarpungar <alvar.pungar@khk.ee>
2015-11-01 17:40:28 +0200

    23. Require [Delivers #103878472]

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+if (isset($_GET['page'])) {
+    require "$_GET[page].php";
+}
+?>
+</body>
+</html>
